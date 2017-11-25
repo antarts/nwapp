1c1b257158456af802d803aaff45daac736a1986
Antarts <m.dream@foxmail.com>
2017-11-25 22:14:16 +0800

    add edit page

--- routes/route.api.js
+++ routes/route.api.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var PostModel = require('../models/post');
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
+router.get('/users', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 /* GET 获取postList数据 */
-router.get('/posts', function(req, res, next) {
+router.get('/posts', function (req, res, next) {
   // res.json({postsList: ['文章1', '文章2', '文章3'] });
   PostModel.find({}, {}, function (err, posts) {
     if (err) {
@@ -20,7 +20,7 @@ router.get('/posts', function(req, res, next) {
 });
 
 /* POST create posts */
-router.post('/posts/create', function(req, res, next) {
+router.post('/posts/create', function (req, res, next) {
   var title = req.body.title;
   var content = req.body.content;
 
@@ -29,12 +29,40 @@ router.post('/posts/create', function(req, res, next) {
   post.content = content;
   post.save(function (err) {
     if (err) {
-      res.json({success: false});
+      res.json({ success: false });
     } else {
-      res.json({success: true});
+      res.json({ success: true });
     }
   });
   // res.send({title, content}); //收到数据后，又把数据返回给了请求方
-} );
+});
+
+/* GET one psot */
+router.get('/posts/one', function (req, res, next) {
+  var id = req.query.id;
+
+  PostModel.findOne({ _id: id }, function (err, post) {
+    if (err) {
+      res.json({ success: false });
+    } else {
+      res.json({ success: true, post });
+    }
+  });
+});
+
+/* PATCH edit post */
+router.post('/posts/edit', function (req, res, next) {
+  var id = req.body.id;
+  var title = req.body.title;
+  var content = req.body.content;
+
+  PostModel.findOneAndUpdate({ _id: id }, { title, content }, function (err) {
+    if (err) {
+      res.json({ success: false });
+    } else {
+      res.json({ success: true });
+    }
+  });
+});
 
 module.exports = router;
--- routes/route.page.js
+++ routes/route.page.js
@@ -4,17 +4,17 @@ var PostModel = require('../models/post');
 var marked = require('marked');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Index page.' });
 });
 
 /* GET posts page. */
-router.get('/posts', function(req, res, next) {
-  res.render('posts', { title: 'post page.'});
+router.get('/posts', function (req, res, next) {
+  res.render('posts', { title: 'post page.' });
 });
 
 /* GET posts edit page. */
-router.get('/posts/create', function(req, res, next) {
+router.get('/posts/create', function (req, res, next) {
   res.render('create');
 });
 
@@ -26,6 +26,13 @@ router.get('/posts/show', function (req, res, next) {
     post.content = marked(post.content);
     res.render('show', { post });
   });
+});
+
+/* GET posts edit page. */
+router.get('/posts/edit', function (req, res, next) {
+  var id = req.query.id;
+
+  res.render('edit', { id });
 });
 
 module.exports = router;
