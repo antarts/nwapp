47a55126938dc100fe8861740abd6237754aedbc
Antarts <m.dream@foxmail.com>
2017-11-25 22:54:18 +0800

    add layouts page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts')
 
 var api = require('./routes/route.api');
 var page = require('./routes/route.page');
@@ -14,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
